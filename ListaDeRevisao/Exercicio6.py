f267031863c2c8f94522229a21496c7406c0a787
Lucas-Holanda <lucasgabryel614@gmail.com>
2026-08-19 23:37:27 +0000

    Update question 6

--- ListaDeRevisao/Exercicio6.py
+++ ListaDeRevisao/Exercicio6.py
@@ -1,1 +1,12 @@
-print("Resultado: 1 -2 3 -4 5 -6 7 -8 9 -10")
+lista = list(map(int, input("Digite os números separados por espaço: ").split()))
+
+def negador(lista):
+    listacopy = []
+    for i in range(len(lista)):
+        if lista[i]%2 == 0:
+            listacopy.append(lista[i]*-1)
+        else:
+            listacopy.append(lista[i])
+    return listacopy
+
+print("Resultado :",*negador(lista))
