2f7f2a73d68d7e7ecd20ea3b6d4916eaacbc49d2
Lucas-Holanda <lucasgabryel614@gmail.com>
2026-08-20 01:48:51 +0000

    Update question 12

---
+++ ListaDeRevisao/Exercicio12.py
@@ -0,0 +1,10 @@
+x = float(input())
+def MenorInteiro(x):
+    y = int(x)
+    res = 0
+    if x == y or x < y:
+        res = y
+    elif x > y:
+        res = y+1
+    return res
+print(MenorInteiro(x))
